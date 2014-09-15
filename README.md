dd0e2ff782c65ec8fb1d91b43cecc426f71ed851
Kyle Manna <kyle@kylemanna.com>
2014-09-14 21:26:05 -0700

    onfailure: Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ systemd-utils
 =============
 
 Random systemd utilities
+
+On Failure
+----------
+
+* Allows users to specify `OnFailure=failure-email@%i.service` under `[Unit]` section of systemd files.
+* The failure-email service will email the user when a service fails unexpectedly and include the `systemd status <svc>` output.
---
+++ onfailure/onfailure.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#
+# Author: Kyle Manna <kyle@kylemanna.com>
+#
+# Simple systemd script used to be called via something like:
+#
+# Example Unit section of a service file:
+#
+# [Unit]
+# ...
+# Onfailure=failure-email@%i.service
+#
+#
+# failure-email@.service:
+#
+# [Unit]
+# Description=OnFailure for %i
+#
+# [Service]
+# Type=oneshot
+# ExecStart=/path/to/onfailure.sh %i
+#
+
+svc=${1-unknown}
+#email=${2-example@gmail.com}
+email=${2-$USER}
+
+cat <<EOF | sendmail -i "$email"
+Subject: [$HOSTNAME] OnFailure Email for $svc
+
+# Status
+
+$(systemctl --user status -l -n 1000 "$svc")
+
+EOF

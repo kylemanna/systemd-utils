8ae869f0a1a717b601cdea73a38e1c93fb021703
Kyle Manna <kyle@kylemanna.com>
2021-05-09 14:10:18 -0700

    gnome: Keyboard scripts

---
+++ scripts/gnome-keyboard-mgr.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+"""Python Lockscreen / Screensaver NumLock Manager for GNOME
+
+Listen to DBus events for screensaver and lockscreen activation and toggle the numlock LED so that it's
+always off when locked and is turned back on when the user deactivates the screen saver and unlocks the
+system.
+"""
+
+import dbus
+import pathlib
+
+from dbus.mainloop.glib import DBusGMainLoop
+from gi.repository import GLib
+
+
+class GnomeLedMgr:
+
+    def __init__(self, device):
+        self.device_path = device
+        self.last_screensaver_active = None
+        self.timeout_timer = None
+
+    def signal_cb(self, screensaver_active):
+
+        if self.timeout_timer:
+            GLib.source_remove(self.timeout_timer)
+            self.timeout_timer = None
+
+        # Convert from dbus.boolean()
+        screensaver_active = bool(screensaver_active)
+
+        # Duplicate signal (not sure why there are two senders of the same signal, the login
+        # manager/screensaver + active user?)
+        if self.last_screensaver_active == screensaver_active:
+            return
+
+        self.last_screensaver_active = screensaver_active
+
+        # Shell hackery: /sys/class/leds/$(readlink /sys/class/input/$(readlink /dev/input/by-id/usb-Logitech_Logitech_G710_Keyboard-event-if01 | awk -F/ '{ print $2 }')/device | awk -F/ '{ print $3}'):white:keys/brightness"
+        event = pathlib.Path(self.device_path).readlink().name
+        input = pathlib.Path(f"/sys/class/input/{event}/device").readlink().name
+
+        if screensaver_active:
+            brightness = 0
+        elif not screensaver_active:
+            brightness = 1
+
+        print(f"Screensaver status = {screensaver_active}, setting brightness {brightness}")
+
+        for key_type in ('keys', 'wasd'):
+            path = pathlib.Path(f"/sys/class/leds/{input}:white:{key_type}/brightness")
+            with path.open('w') as f:
+                f.write(f"{brightness}")
+
+    def timeout_leds(self):
+        """Called when the user fails to complete the login and turns LEDs back off after the screen is
+        woken by the WakeUpScreen signal.
+        """
+        print(f"Wake Signal timeout")
+        self.signal_cb(True)
+        return False
+
+    def wake_signal_cb(self):
+        """Detect the screens turning on to enable keyboard LEDs"""
+        print(f"Wake Signal received")
+        self.timeout_timer = GLib.timeout_add_seconds(2, self.timeout_leds)
+        self.signal_cb(False)
+
+if __name__ == '__main__':
+
+
+    DBusGMainLoop(set_as_default=True)
+    bus = dbus.SessionBus()
+
+    mgr = GnomeLedMgr('/dev/input/by-id/usb-Logitech_Logitech_G710_Keyboard-event-if01')
+
+    bus.add_signal_receiver(mgr.signal_cb, 'ActiveChanged', 'org.gnome.ScreenSaver')
+
+    # TODO: THis doesn't fire when monitors turn on, but when the screensave is unlocked, kind of
+    # useless.
+    bus.add_signal_receiver(mgr.wake_signal_cb, 'WakeUpScreen', 'org.gnome.ScreenSaver')
+
+    loop = GLib.MainLoop()
+    loop.run()
--- scripts/gnome-numlock-mgr.py
+++ scripts/gnome-numlock-mgr.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-"""Python Lockscreen / Screensaver Numlock Manager for GNOME
+"""Python Lockscreen / Screensaver NumLock Manager for GNOME
 
 Listen to DBus events for screensaver and lockscreen activation and toggle the numlock LED so that it's
 always off when locked and is turned back on when the user deactivates the screen saver and unlocks the
@@ -12,41 +12,92 @@ import evdev
 from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib
 
-def toggle_numlock(dev):
+
+class GnomeNumlockMgr:
+    ui = None
     e = evdev.ecodes
 
-    # Seems that toggling numlock on the lock screen fails to turn off the LED, perhaps because the
-    # terminal has changed and the user input fails?  Will the LED and internal numlock state ever get
-    # out of sync?
-    # FIXME get rid of the following when it's better understood.
-    dev.set_led(e.LED_NUML, 0)
+    def __init__(self, device):
+        self.device_path = device
+        #self.dev = evdev.InputDevice(device)
+        # Open on demand to avoid this strange issue on first boot:
+        # Traceback (most recent call last):
+        #  File "/usr/lib/python3.8/site-packages/dbus/connection.py", line 232, in maybe_handle_message
+        #    self._handler(*args, **kwargs)
+        #  File "/home/nitro/.config/systemd/utils/scripts/gnome-numlock-mgr.py", line 59, in signal_cb
+        #    if screensaver_active and self.get_numlock():
+        #  File "/home/nitro/.config/systemd/utils/scripts/gnome-numlock-mgr.py", line 50, in get_numlock
+        #    return self.e.LED_NUML in self.dev.leds()
+        #  File "/usr/lib/python3.8/site-packages/evdev/device.py", line 262, in leds
+        #    leds = _input.ioctl_EVIOCG_bits(self.fd, ecodes.EV_LED)
+        #SystemError: <built-in function ioctl_EVIOCG_bits> returned NULL without setting an error
+        self.dev = None
+        self.cleared_numlock = False
 
-    ui.write(e.EV_KEY, e.KEY_NUMLOCK, 1)
-    ui.write(e.EV_KEY, e.KEY_NUMLOCK, 0)
-    ui.syn()
 
-def get_numlock(dev):
-    return evdev.ecodes.LED_NUML in dev.leds()
+    def ui_thing(self):
+        pass
+        # Toggling numlock while the screensaver activates due to timeout will cause it to reset and
+        # instantly wake and never sleep!  Don't do this.
+        #if not self.ui:
+        #    self.ui = evdev.UInput(name='GNOME NumLock Manager')
+        #    print(f"Created {self.ui}")
+        #
+        #self.ui.write(e.EV_KEY, e.KEY_NUMLOCK, 1)
+        #self.ui.write(e.EV_KEY, e.KEY_NUMLOCK, 0)
+        #self.ui.syn()
 
-def signal_cb(screensaver_active):
-    # Convert from dbus.boolean()
-    screensaver_active = bool(screensaver_active)
-    print(f"Screensaver status = {screensaver_active}")
+    def init_dev(self):
+        if not self.dev:
+            self.dev = evdev.InputDevice(self.device_path)
+            print(f"Opened {self.dev}")
 
-    if screensaver_active == get_numlock(dev):
-        print('Toggling NUMLOCK')
-        toggle_numlock(dev)
+        # HACK Test until I can figure out why this needs to happen
+        try:
+            leds = self.dev.leds()
+        except Exception as e:
+            print(f"Failed to get LEDs, re-opening...")
+            self.dev = evdev.InputDevice(self.device_path)
+
+
+    def set_numlock(self, val = 1):
+        # Hack, why?
+        self.init_dev()
+        # Seems that toggling numlock on the lock screen fails to turn off the LED, perhaps because the
+        # terminal has changed and the user input fails?  Will the LED and internal numlock state ever get
+        # out of sync?
+        # FIXME get rid of the following when it's better understood.
+        self.dev.set_led(self.e.LED_NUML, val)
+        self.cleared_numlock = not bool(val)
+
+
+    def get_numlock(self):
+        # Hack, why?
+        self.init_dev()
+        return self.e.LED_NUML in self.dev.leds()
+
+    def signal_cb(self, screensaver_active):
+        # Convert from dbus.boolean()
+        screensaver_active = bool(screensaver_active)
+        numlock = None
+
+        if screensaver_active and self.get_numlock():
+            numlock = 0
+        elif not screensaver_active and self.cleared_numlock:
+            numlock = 1
+
+        print(f"Screensaver status = {screensaver_active}, setting NumLock {numlock}")
+
+        if numlock != None:
+            self.set_numlock(numlock)
 
 if __name__ == '__main__':
 
-    dev = evdev.InputDevice('/dev/input/by-id/usb-Logitech_Logitech_G710_Keyboard-event-kbd')
-    print(f"Opened {dev}")
-    ui = evdev.UInput(name='GNOME Numlock Manager')
-    print(f"Created {ui}")
+    mgr = GnomeNumlockMgr('/dev/input/by-id/usb-Logitech_Logitech_G710_Keyboard-event-kbd')
 
     DBusGMainLoop(set_as_default=True)
     bus = dbus.SessionBus()
-    bus.add_signal_receiver(signal_cb, 'ActiveChanged', 'org.gnome.ScreenSaver')
+    bus.add_signal_receiver(mgr.signal_cb, 'ActiveChanged', 'org.gnome.ScreenSaver')
 
     loop = GLib.MainLoop()
     loop.run()
